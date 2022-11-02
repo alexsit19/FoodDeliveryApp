e9e1f7cbfc7090decdb1c161d25583a2d3255778
alexsit19 <alexsit12@gmail.com>
2022-11-03 00:27:41 +0300

    add navigation and structure

--- app/src/main/java/com/example/fooddeliveryapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/fooddeliveryapp/presentation/MainActivity.kt
@@ -1,19 +1,14 @@
 package com.example.fooddeliveryapp.presentation
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
-import com.example.fooddeliveryapp.BuildConfig
-import com.example.fooddeliveryapp.data.DefaultRepository
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.example.fooddeliveryapp.R
 import com.example.fooddeliveryapp.databinding.ActivityMainBinding
 import com.example.fooddeliveryapp.databinding.ActivityMainBinding.inflate
-import com.google.android.material.tabs.TabLayoutMediator
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import java.io.IOException
 
 
 class MainActivity : AppCompatActivity() {
@@ -25,30 +20,41 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         _binding = inflate(layoutInflater)
         setContentView(binding.root)
-        setupUi()
+
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+
+        val navController = navHostFragment.navController
+        val bottomNavigation = binding.bottomNavigation
+        bottomNavigation.setupWithNavController(navController)
+
+        //setupUi()
     }
 
-    @Throws(IOException::class)
-    private fun getBitmapFromAsset(fileName: String): Bitmap? {
-        val assetManager = assets
-        val inputStream = assetManager.open(fileName)
-        return BitmapFactory.decodeStream(inputStream)
-    }
-
-    private fun setupUi() {
-        val banner1: Bitmap? = getBitmapFromAsset("banner_b1.png")
-        val banner2: Bitmap? = getBitmapFromAsset("banner_b2.png")
-        val banner3: Bitmap? = getBitmapFromAsset("banner_b3.png")
-        val banner4: Bitmap? = getBitmapFromAsset("banner_b4.png")
-        val banner5: Bitmap? = getBitmapFromAsset("banner_b5.png")
-        val banners: List<Bitmap?> = listOf(banner1, banner2, banner3, banner4, banner5)
-
-        val bannersAdapter = BannersAdapter(banners)
-
-        binding.banners.adapter = bannersAdapter
-        TabLayoutMediator(binding.tabLayout, binding.banners) { _, _ ->
-
-        }.attach()
-
-    }
+//    @Throws(IOException::class)
+//    private fun getBitmapFromAsset(fileName: String): Bitmap? {
+//        val assetManager = assets
+//        val inputStream = assetManager.open(fileName)
+//        return BitmapFactory.decodeStream(inputStream)
+//    }
+//
+//    private fun setupUi() {
+//        val banner1: Bitmap? = getBitmapFromAsset("banner_b1.png")
+//        val banner2: Bitmap? = getBitmapFromAsset("banner_b2.png")
+//        val banner3: Bitmap? = getBitmapFromAsset("banner_b3.png")
+//        val banner4: Bitmap? = getBitmapFromAsset("banner_b4.png")
+//        val banner5: Bitmap? = getBitmapFromAsset("banner_b5.png")
+//        val banners: List<Bitmap?> = listOf(banner1, banner2, banner3, banner4, banner5)
+//
+//        val bannersAdapter = BannersAdapter(banners)
+//
+//        binding.banners.adapter = bannersAdapter
+//        TabLayoutMediator(binding.tabLayout, binding.banners) { _, _ ->
+//
+//        }.attach()
+//
+//
+//
+//    }
 }
